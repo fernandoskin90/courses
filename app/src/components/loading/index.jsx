4553c5382df72e417661b3a812018de6f0fb4dfc
fernando guerrero campos <fernandoguerreroc65@gmail.com>
2020-07-10 21:37:48 -0500

    updated loading

--- app/src/components/loading/index.jsx
+++ app/src/components/loading/index.jsx
@@ -5,7 +5,7 @@ export default function Loading() {
   return (
     <div className="loading-content">
       <em className="fad fa-spinner" />
-      <span>Loading</span>
+      <span>Loading...</span>
     </div>
   );
 }
